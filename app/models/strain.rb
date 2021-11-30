25c98c42f2600ddc3627030e12d49fed87b26670
poolug <pauluzcategui43@gmail.com>
2021-11-29 21:56:44 -0300

    Add testing spec 1.0

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -2,4 +2,5 @@ class Strain < ApplicationRecord
     validates :name, presence: :true
     has_many :wines_strains
     has_many :wines, through: :wines_strains
+    validates :name, uniqueness: true
 end
